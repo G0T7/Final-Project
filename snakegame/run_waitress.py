eb72d04582a3a9a8e7efa7c8535c8355ef9f531f
GOT7 <darkroast20@gmail.com>
2024-09-06 06:06:22 +0400

    first commit

--- snakegame/run_waitress.py
+++ snakegame/run_waitress.py
@@ -1,5 +1,5 @@
 from waitress import serve
-from backend.wsgi import application
+from backend.wsgi import application  # Ensure this matches your wsgi.py module
 
 if __name__ == '__main__':
     serve(application, host='0.0.0.0', port=8000)
