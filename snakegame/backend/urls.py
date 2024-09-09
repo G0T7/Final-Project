4d5e743d06b68f2a957be1f3995499f11e3fc6db
GOT7 <darkroast20@gmail.com>
2024-09-09 16:21:40 +0400

    first commit

--- snakegame/backend/urls.py
+++ snakegame/backend/urls.py
@@ -4,8 +4,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('game.urls')),  # Include app-specific URLs
+    path('', TemplateView.as_view(template_name='index.html')),  # Serve your React app
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
