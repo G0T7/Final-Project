97843544344ab52c57f2794b833b87c5237e9ffb
GOT7 <darkroast20@gmail.com>
2024-09-12 01:58:11 +0400

    first commit

--- snakegame/backend/urls.py
+++ snakegame/backend/urls.py
@@ -6,10 +6,5 @@ from game import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/signup/', views.signup_view, name='signup'),
-    path('api/login/', views.login_view, name='login'),
-    path('api/user-profile/', views.user_profile_view, name='user-profile'),
-    path('api/leaderboard/', views.leaderboard_view, name='leaderboard'),
-    # Ensure this is included correctly and does not override the above paths
-    path('api/', include('game.urls')),  
+    path('api/', include('game.urls')),  # Ensure this line is included
 ]
