b73509a1fd6cf2a12598fd28e03e7b7954a90235
GOT7 <darkroast20@gmail.com>
2024-09-12 03:48:02 +0400

    first commit

--- snakegame/game/urls.py
+++ snakegame/game/urls.py
@@ -1,23 +1,16 @@
 # game/urls.py
 
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
-
-router = DefaultRouter()
-router.register(r'game-scores', views.GameScoreViewSet)
-router.register(r'user-profiles', views.UserProfileViewSet)
-router.register(r'users', views.UserViewSet)
+from django.urls import path
+from .views import signup_view, login_view, user_profile_view, leaderboard_view, submit_score, ai_move, start_game, restart_game, save_score
 
 urlpatterns = [
-    path('', include(router.urls)),  # Main API routes
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('user-profile/', views.user_profile_view, name='user_profile'),
-    path('leaderboard/', views.leaderboard_view, name='leaderboard'),
-    path('submit-score/', views.submit_score, name='submit_score'),
-    path('ai-move/', views.ai_move, name='ai_move'),  # Ensure this line is present
-    path('start-game/', views.start_game, name='start_game'),
-    path('restart-game/', views.restart_game, name='restart_game'),
-    path('save_score/', views.save_score, name='save_score'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('user-profile/', user_profile_view, name='user_profile'),
+    path('leaderboard/', leaderboard_view, name='leaderboard'),
+    path('submit-score/', submit_score, name='submit_score'),
+    path('ai-move/', ai_move, name='ai_move'),
+    path('start-game/', start_game, name='start_game'),
+    path('restart-game/', restart_game, name='restart_game'),
+    path('save_score/', save_score, name='save_score'),
 ]
