502c0e41c43c67710672406320d4eb0b3aaf943d
GOT7 <darkroast20@gmail.com>
2024-09-12 03:04:30 +0400

    first commit

--- snakegame/backend/settings.py
+++ snakegame/backend/settings.py
@@ -47,7 +47,8 @@ MIDDLEWARE = [
 # CORS configuration
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React development server
-    "https://retro-mini-snake-game-obxi.onrender.com",  # Production
+    "https://retro-mini-snake-game-obxi.onrender.com", 
+    "https://retro-mini-snake-game.onrender.com", # Production
 ]
 
 # URL configuration
